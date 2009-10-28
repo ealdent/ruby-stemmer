2ffab7712046f8a076513f7235a135050365e9b9
aurelian <oancea@gmail.com>
2009-10-28 16:44:45 +0100

    Added Yury's changes to extconf

--- ext/lingua/extconf.rb
+++ ext/lingua/extconf.rb
@@ -1,9 +1,11 @@
+ENV['RC_ARCHS'] = '' if RUBY_PLATFORM =~ /universal-darwin/
 require "mkmf"
 
 ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 LIBSTEMMER = File.join(ROOT, 'libstemmer_c')
 
 # build libstemmer_c
+ENV['ARCHFLAGS']= "-arch #{Config::CONFIG['host_cpu']}" if RUBY_PLATFORM =~ /darwin/
 ENV['ARCHFLAGS']= "-arch x86_64" if Config::CONFIG['host_cpu'] == 'i686' && RUBY_PLATFORM =~ /darwin/
 system "cd #{LIBSTEMMER}; make libstemmer.o; cd #{ROOT};"
 
