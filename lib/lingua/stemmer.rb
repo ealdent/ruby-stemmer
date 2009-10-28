dab42e5442d832a566efa12820c09bc37bfd0d36
aurelian <oancea@gmail.com>
2009-10-28 18:18:53 +0100

    Created Lingua.stemmer method to handle stemming of Arrays too

--- lib/lingua/stemmer.rb
+++ lib/lingua/stemmer.rb
@@ -1,11 +1,25 @@
+require 'yaml'
+require 'lingua/stemmer_native'
+
 module Lingua
-  VERSION
 
   def self.stemmer(o, options={})
-    @stemmer ||= Stemmer.new(  )
+    _stemmer= Stemmer.new({:language => "en", :encoding => "UTF_8"}.merge(options))
+    words= o.kind_of?(Array)? o.map{|e|e.to_s} : [o.to_s]
+    results = [] unless block_given?
+    words.each do | word |
+      result = _stemmer.stem(word)
+      if block_given?
+        yield result
+      else
+        results << result
+      end
+    end
+    return (results.length == 1)? results[0] : results unless block_given?
+    _stemmer
   end
 
   class Stemmer
-
+    VERSION = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "VERSION")))
   end
 end
